e7aa2563dcd8ec1b0e9479bdd3e5e97e81fd403c
iolav <120427939+iolav@users.noreply.github.com>
2025-02-12 12:03:09 -0600

    parser

--- LangInterpreterWASH/Parser.cs
+++ LangInterpreterWASH/Parser.cs
@@ -1,7 +1,72 @@
-class ASTNode(string A, Token V, Token L, Token R) { // Node class to store things for the AST
-    public string Action = A;
+using System.Collections;
+using System.Data;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
 
-    public Token Value = V;
-    public Token Left = L;
-    public Token Right = R;
+class ASTNode(string A, string V, ASTNode? L, ASTNode? R) { // Node class to store things for the AST
+    public string Action = A;
+    public string Value = V;
+    public ASTNode? Left = L;
+    public ASTNode? Right = R;
+}
+
+class Parser(Queue<Token> TokenQueue) {
+    public ASTNode Parse() {
+        return Expression();
+    }
+
+    private ASTNode Expression() {
+        ASTNode Node = Term();
+
+        while (Peek() == "Plus" || Peek() == "Minus") {
+            Token Operator = Dequeue();
+            Node = new ASTNode(Operator.Classifier, Operator.Value, Node, Term());
+        }
+
+        return Node;
+    }
+
+    private ASTNode Term() {
+        ASTNode Node = Factor();
+
+        while (Peek() == "Multiply" || Peek() == "Divide") {
+            Token Operator = Dequeue();
+            Node = new ASTNode(Operator.Classifier, Operator.Value, Node, Factor());
+        }
+        
+        return Node;
+    }
+
+    private ASTNode Factor() {
+        if (Peek() == "Number")
+            return new ASTNode("Number", Dequeue().Value, null, null);
+        else if (Peek() == "LParen") {
+            Dequeue();
+
+            ASTNode Node = Expression();
+
+            if (Peek() != "RParen")
+                throw new Exception();
+            
+            Dequeue();
+
+            return Node;
+        }
+
+        throw new Exception();
+    }
+
+    private string? Peek() {
+        if (TokenQueue.Count > 0)
+            return TokenQueue.Peek().Classifier;
+
+        return null;
+    }
+
+    private Token Dequeue() {
+        if (TokenQueue.Count > 0)
+            return TokenQueue.Dequeue();
+        
+        throw new Exception();
+    }
 }
--- LangInterpreterWASH/obj/Debug/net8.0/LangInterpreterWASH.AssemblyInfo.cs
+++ LangInterpreterWASH/obj/Debug/net8.0/LangInterpreterWASH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cabf8fd3f2b95a63b07f0e96f75816d0e0d6d6eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b81267bcf13ab0eea07d4d6be4150b38589905")]
 [assembly: System.Reflection.AssemblyProductAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
