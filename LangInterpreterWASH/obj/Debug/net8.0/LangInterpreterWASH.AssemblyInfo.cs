736b1820eb4574cd2fdb5fdd32dcb18103358678
iolav <120427939+iolav@users.noreply.github.com>
2025-03-26 15:55:57 -0500

    temp

--- LangInterpreterWASH/obj/Debug/net8.0/LangInterpreterWASH.AssemblyInfo.cs
+++ LangInterpreterWASH/obj/Debug/net8.0/LangInterpreterWASH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a9b8a92c08fcaaa028d08e1a6189cc50349d59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a42b0644e1b47d0ebcaec3f51234feb85c22298")]
 [assembly: System.Reflection.AssemblyProductAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LangInterpreterWASH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
